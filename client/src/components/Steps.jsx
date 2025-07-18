912fe52ab278249d7e93ce64104da45a132458dd
Raj-Mayank2 <mayank170403@gmail.com>
2025-07-18 23:53:18 +0530

    make changes

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -5,7 +5,7 @@ const Steps = () => {
     return (
         <div className='mx-4 lg:mx-44 py-20 xl:py-40 '>
 
-            <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Steps to remove background <br /> image in seconds.</h1>
+            <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Steps to vanish your background <br /> image in seconds.</h1>
 
             <div className=' flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
 
@@ -13,7 +13,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.upload_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Upload image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Upload your image to change <br /> This is the first step</p>
                     </div>
                 </div>
 
@@ -21,7 +21,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Remove background</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>We will change your image for better <br /> This is a for you only</p>
                     </div>
                 </div>
 
@@ -29,7 +29,7 @@ const Steps = () => {
                     <img className='max-w-9' src={assets.download_icon} alt="" />
                     <div>
                         <p className='text-xl font-medium'>Download image</p>
-                        <p className='text-sm text-neutral-500 mt-1'>This is a demo text, will replace it later. <br /> This is a demo..</p>
+                        <p className='text-sm text-neutral-500 mt-1'>Dwownload your changed image<br /> This is the final step.</p>
                     </div>
                 </div>
 
